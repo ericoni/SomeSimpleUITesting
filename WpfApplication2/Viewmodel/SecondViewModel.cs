04b1563f306082475c3cad7ad425349260f6d6b3
admin <admin@W10ENT>
2019-06-07 15:47:16 +0200

    Add project files.

---
+++ WpfApplication2/Viewmodel/SecondViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication2.Viewmodel
+{
+	public class SecondViewModel : ViewModelBase
+	{
+		string testString = null;
+		public SecondViewModel()
+		{
+			testString = "testiranje";
+		}
+		public string TestString
+		{
+			get { return testString; }
+			set
+			{
+				if (testString != value)
+				{
+					testString = value;
+					OnPropertyChanged("TestString");
+				}
+			}
+		}
+	}
+}
