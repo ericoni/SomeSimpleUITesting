175df9b69c1f48dda107727ade583d20ae1fa0d6
Dragan Eric <ericoni94@gmail.com>
2019-08-06 15:19:32 +0200

    Not much.

---
+++ WpfApplicationTestiranje/Converters/AlarmBlinkConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfApplicationTestiranje.Converters
+{
+	public class AlarmBlinkConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if ((int)value > 0)
+			{
+				return FetchStyle("FaderStyleButton");
+			}
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		Style FetchStyle(string name)
+		{
+			return Application.Current.Resources[name] as Style;
+		}
+	}
+}
